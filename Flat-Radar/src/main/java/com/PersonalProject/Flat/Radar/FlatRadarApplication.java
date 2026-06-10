495eafb7da0821f37e9b40f006ec92af0fb85248
Naveen <naveenkumarpathak9@gmail.com>
2026-06-11 00:27:40 +0530

    Project Initalization
    Spring boot project is created

---
+++ Flat-Radar/src/main/java/com/PersonalProject/Flat/Radar/FlatRadarApplication.java
@@ -0,0 +1,13 @@
+package com.PersonalProject.Flat.Radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatRadarApplication.class, args);
+	}
+
+}
---
+++ Flat-Radar/src/test/java/com/PersonalProject/Flat/Radar/FlatRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PersonalProject.Flat.Radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

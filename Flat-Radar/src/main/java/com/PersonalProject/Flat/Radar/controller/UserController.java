c17361c1de2975fb517240b5a45954e3df131e34
Naveen <naveenkumarpathak9@gmail.com>
2026-06-21 13:25:07 +0530

    Ready for Deployemnt V1
    login and registeration are reay for deployment

--- Flat-Radar/src/main/java/com/PersonalProject/Flat/Radar/controller/UserController.java
+++ Flat-Radar/src/main/java/com/PersonalProject/Flat/Radar/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-//@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
